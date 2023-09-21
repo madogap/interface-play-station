6ff45c6fd7be339672104c145827381ecb4cc627
madogap <madogap@gmail.com>
2023-09-21 15:50:34 -0300

    Creating a repository for a PlayStation interface and creating components that will satisfy needs

---
+++ src/app/games-data.ts
@@ -0,0 +1,29 @@
+// src/app/games-data.ts
+
+export const GAMES = [
+  {
+    id: 1,
+    title: 'The Last of Us Part II',
+    price: 59.99,
+    image: 'last-of-us.jpg',
+  },
+  {
+    id: 2,
+    title: 'The Last of Us Part III',
+    price: 39.99,
+    image: 'last-of-us.jpg',
+  },
+  {
+    id: 3,
+    title: 'The Last of Us Part IV',
+    price: 99.99,
+    image: 'last-of-us.jpg',
+  },
+  {
+    id: 4,
+    title: 'The Last of Us Part V',
+    price: 69.99,
+    image: 'last-of-us.jpg',
+  },
+  // Adicione mais jogos aqui
+];
