9c9c05a36833adf3b83ed954534fa06a2af7e582
madogap <madogap@gmail.com>
2023-09-21 18:12:17 -0300

    Ready and prepared for delivery

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent {
+  @Input()
+	gameCover:string = ""
+	@Input()
+	gameLabel:string=""
+	@Input()
+	gameType:string =""
+	@Input()
+	gamePrice:string = ""
 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 }
